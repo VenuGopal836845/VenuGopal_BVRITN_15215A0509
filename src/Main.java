4442e5ae6c337a9ddc8dd300d0b066332126402d
VenuGopal836845 <31851586+VenuGopal836845@users.noreply.github.com>
2017-09-12 00:58:37 -0700

    check

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
 	public static void main(String[] args) 
 	{
-		
+		Student a= new Student(12,"Venu",2001-01-02,75);
+		a.setId(12);
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before vffsfto submit
